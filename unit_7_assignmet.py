170671fa22ff349fc64359440964a3079bb6b45b
scout <scout.crooke@ssfs.org>
2019-11-11 14:14:22 -0500

    worked on assignment 7

---
+++ unit_7_assignmet.py
@@ -0,0 +1,8 @@
+# Scout Crooke, 11/11/19, this code encodes and decodes the user's word
+
+word = str(input("what word do you want to encode?"))
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+shift = int(input("How many times do you want to shift the alphabet?"))
+
+
+
