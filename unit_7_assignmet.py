f2f089014852e3d2ed2ffd950c138a1d45ead222
scout <scout.crooke@ssfs.org>
2019-11-12 15:09:21 -0500

    worked more on unit 7 assignment option 1

--- unit_7_assignmet.py
+++ unit_7_assignmet.py
@@ -1,8 +1,31 @@
 # Scout Crooke, 11/11/19, this code encodes and decodes the user's word
 
-word = str(input("what word do you want to encode?"))
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-shift = int(input("How many times do you want to shift the alphabet?"))
+ask = input("Press e to encode, d to decode, or q to quit")
 
 
+def shifted():
+    shift = int(input("How many times do you want to shift the alphabet?"))
+    first = alphabet[0:shift]
+    last = alphabet[shift:]
+    final = last + first
+    return final
+
+
+def encode(new_alphabet):
+    word = (input("what word do you want to encode?"))
+    word = word.lower()
+    code_word = " "
+    for x in word:
+        position = alphabet.index(x)
+        new_position = new_alphabet[position]
+        print("your encoded word is", new_position)
+
+
+def main():
+    new_alphabet = shifted()
+    encode(new_alphabet)
+
+
+main()
 
